4f17b7537903d16982468210b0ef584a0746bef7
jzad <github@jzadl.xyz>
2026-05-24 23:52:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,6 +50,14 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
